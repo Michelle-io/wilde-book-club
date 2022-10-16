6c83a4aa6250f7be8ecc62fa6b333de780ee69de
Michelle McCann <105986556+Michelle-io@users.noreply.github.com>
2022-10-16 23:15:14 +0100

    added javaScript file

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector('#menu-button');
+const navMenu = document.querySelector('#nav-menu');
+
+ // Add event listener to menu button
+menuButton.addEventListener('click', function(event){
+  // Toggle classes on click
+  menuButton.classList.toggle('menu-button-active')
+  navMenu.classList.toggle('menu-active')
+})
