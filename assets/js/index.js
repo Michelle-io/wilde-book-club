b6daf2f00dceabee21f57865479e5177e6b4b861
Michelle McCann <105986556+Michelle-io@users.noreply.github.com>
2022-10-17 01:40:24 +0100

    fix js hint missing semi semicolon warning

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
-const menuButton = document.querySelector('#menu-button');
-const navMenu = document.querySelector('#nav-menu');
+const menuButton = document.querySelector("#menu-button");
+const navMenu = document.querySelector("#nav-menu");
 
- // Add event listener to menu button
-menuButton.addEventListener('click', function(event){
+// Add event listener to menu button
+menuButton.addEventListener("click", function (event) {
   // Toggle classes on click
-  menuButton.classList.toggle('menu-button-active')
-  navMenu.classList.toggle('menu-active')
-})
+  menuButton.classList.toggle("menu-button-active");
+  navMenu.classList.toggle("menu-active");
+});
